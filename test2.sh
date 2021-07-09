58ddd5d38ab6f9e058a8dad9356fcd90d81bccb8
Shihao Dong <38196678+LouisDong1995@users.noreply.github.com>
2021-07-09 15:54:34 +0800

    Delete test2.sh

--- test2.sh
+++
@@ -1,4 +0,0 @@
-cd MDECfusion
-CUDA_VISIBLE_DEVICES=1 python AE_VAE.py --dataset mnist --save_dir ../results/ae_vae/mnist/1
-CUDA_VISIBLE_DEVICES=1 python AE_VAE.py --dataset mnist --save_dir ../results/ae_vae/mnist/2
-CUDA_VISIBLE_DEVICES=1 python AE_VAE.py --dataset mnist --save_dir ../results/ae_vae/mnist/3
