00d60b6a8cdcc285ad36b427f88060e0f83d02d0
Shihao Dong <38196678+LouisDong1995@users.noreply.github.com>
2021-07-09 15:54:48 +0800

    Delete test1.sh

--- test1.sh
+++
@@ -1,4 +0,0 @@
-cd MDECfusion
-python AE_CAE.py --dataset mnist --save_dir ../results/ae_cae/mnist/1
-python AE_CAE.py --dataset mnist --save_dir ../results/ae_cae/mnist/2
-python AE_CAE.py --dataset mnist --save_dir ../results/ae_cae/mnist/3
