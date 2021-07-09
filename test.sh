421085b006acb15be4f414b72acb21705747df4e
Shihao Dong <38196678+LouisDong1995@users.noreply.github.com>
2021-07-09 15:56:21 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,24 @@
-#cd DEC
-#python DEC.py --dataset fashion_mnist --save_dir ../results/dec/fashion_mnist/1
-#python DEC.py --dataset fashion_mnist --save_dir ../results/dec/fashion_mnist/2
-#python DEC.py --dataset fashion_mnist --save_dir ../results/dec/fashion_mnist/3
-#cd ..
+cd DEC
+python DEC.py --dataset fashion_mnist --save_dir ../results/dec/fashion_mnist
 
-#cd IDEC
-##python IDEC.py --dataset fashion_mnist --ae_weights ../results/dec/fashion_mnist/1/ae_weights.h5 --save_dir ../results/idec/fashion_mnist/1
-#python IDEC.py --dataset fashion_mnist --ae_weights ../results/dec/fashion_mnist/2/ae_weights.h5 --save_dir ../results/idec/fashion_mnist/2
-#python IDEC.py --dataset fashion_mnist --ae_weights ../results/dec/fashion_mnist/3/ae_weights.h5 --save_dir ../results/idec/fashion_mnist/3
-#cd ..
+cd ..
 
-#cd DCEC
-#python DCEC.py --dataset fashion_mnist --save_dir ../results/dcec/fashion_mnist/1
-#python DCEC.py --dataset fashion_mnist --save_dir ../results/dcec/fashion_mnist/2
-#python DCEC.py --dataset fashion_mnist --save_dir ../results/dcec/fashion_mnist/3
-#cd ..
-#
-#cd DVAE
-#python DVAE.py --dataset fashion_mnist --save_dir ../results/dvae/fashion_mnist/1
-#python DVAE.py --dataset fashion_mnist --save_dir ../results/dvae/fashion_mnist/2
-#python DVAE.py --dataset fashion_mnist --save_dir ../results/dvae/fashion_mnist/3
-#cd ..
-#
-#cd MDECfusion
-#python MDECfusion.py --dataset fashion_mnist --save_dir ../results/mdecfusion/fashion_mnist/1
-#python MDECfusion.py --dataset fashion_mnist --save_dir ../results/mdecfusion/fashion_mnist/2
-#python MDECfusion.py --dataset fashion_mnist --save_dir ../results/mdecfusion/fashion_mnist/3
+cd IDEC
+python IDEC.py --dataset fashion_mnist --ae_weights ../results/dec/fashion_mnist/1/ae_weights.h5 --save_dir ../results/idec/fashion_mnist
+
+cd ..
+
+cd DCEC
+python DCEC.py --dataset fashion_mnist --save_dir ../results/dcec/fashion_mnist
+
+cd ..
+
+cd DVAE
+python DVAE.py --dataset fashion_mnist --save_dir ../results/dvae/fashion_mnist
+
+cd ..
+
+cd MDECfusion
+python MDECfusion.py --dataset fashion_mnist --save_dir ../results/mdecfusion/fashion_mnist
+
 
